3c81d48ad2e146e9dff2a230693ea128646ee778
Luja Maharjan <lujawmrz@gmail.com>
2025-04-02 14:31:11 +0545

    update header and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,75 +1,74 @@
-import React from "react";
-import {
-  FaBook,
-  FaHome,
-  FaSearch,
-  FaSignInAlt,
-  FaUserCircle,
-} from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
-    <header className="bg-slate-200 shadow-md ">
-      <div className="max-w-6xl mx-auto flex items-center justify-between p-3">
-        {/* Firm Name */}
-        <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span>Sherpa</span>
-            <span className="text-red-600">Dai</span>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-red-500'>Sherpa</span>
+            <span className='text-black'>Dai</span>
           </h1>
         </Link>
-
-        {/* Search Bar */}
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
           <input
-            type="text"
-            placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-500" />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
-
-        <Link to="/profile"></Link>
-
-        <div className="flex items-center gap-4">
-          <Link to="/">
-            <button
-              className="flex items-center text-slate-500 hover:text-sky-600"
-              li
-            >
-              <FaHome size={24} />
-              <span className="ml-2 hidden sm:inline">Home</span>
-            </button>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
           </Link>
-
-          {/* Profile Icon */}
-          <Link to="/profile">
-            <button
-              className="flex items-center text-slate-500 hover:text-sky-600"
-              li
-            >
-              <FaUserCircle size={24} />
-              <span className="ml-2 hidden sm:inline">Profile</span>
-            </button>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
           </Link>
-
-          {/* Sign-In Icon */}
-          <Link to="/sign-in">
-            <button className="flex items-center text-slate-500 hover:text-sky-600">
-              <FaSignInAlt size={24} />
-              <span className="ml-2 hidden sm:inline">Sign In</span>
-            </button>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
           </Link>
-
-          {/* About icon */}
-          <Link to="/about">
-            <button className="flex items-center text-slate-500 hover:text-sky-600">
-              <FaBook size={24} />
-              <span className="ml-2 hidden sm:inline">About</span>
-            </button>
-          </Link>
-        </div>
+        </ul>
       </div>
     </header>
   );
